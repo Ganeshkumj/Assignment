65921a46540787c4ae52f170d74dcedadb028846
Ganeshkumj <102136764+Ganeshkumj@users.noreply.github.com>
2022-05-03 18:23:48 +0530

    java assignment solutions
    java assignment solutions

--- question3.java
+++ question3.java
@@ -1,77 +1,48 @@
-import java.util.Arrays;
-import java.util.List;
 
 public class question3 {
-	public static void main(String args[])
+public static void main(String args[])
+{
+	person1 p1=new person1("name1","name2",'M');
+	System.out.println("details of person class are mentioned below");
+	System.out.println(p1.getFirstName());
+	System.out.println(p1.getLastName());
+	System.out.println(p1.getGender());
+}
+}
+
+class person1{
+	String FirstName;
+	String LastName;
+	char gender;
+	
+	public person1(String firstName, String lastName, char gender) 
 	{
-	Trader raoul = new Trader("Raoul", "Cambridge");
-	Trader mario = new Trader("Mario","Milan");
-	Trader alan = new Trader("Alan","Cambridge");
-	Trader brian = new Trader("Brian","Cambridge");
-
-	List<Transaction> transactions = Arrays.asList(
-		new Transaction(brian, 2011, 300),
-		new Transaction(raoul, 2012, 1000),
-		new Transaction(raoul, 2011, 400),
-		new Transaction(mario, 2012, 710),
-		new Transaction(mario, 2012, 700),
-		new Transaction(alan, 2012, 950)
-	      );
+		super();
+		FirstName = firstName;
+		LastName = lastName;
+		this.gender = gender;
 	}
-}
- class Trader {
-	 
-    private String name;
-    private String city;
-    Trader(String name,String city)
-    {
-    	super();
-    	this.name=name;
-    	this.city=city;
-    }
-	public String getName() {
-		return name;
+	public person1()
+	{
+	System.out.println("this is a default constructor");	
 	}
-	public void setName(String name) {
-		this.name = name;
+	public String getFirstName() {
+		return FirstName;
 	}
-	public String getCity() {
-		return city;
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
 	}
-	public void setCity(String city) {
-		this.city = city;
+	public String getLastName() {
+		return LastName;
 	}
-
-}
- class Transaction {
-
-    private Trader trader;
-    private int year;
-    private int value;
-    
-    Transaction(Trader trader,int year,int value)
-    {
-    	super();
-    	this.trader=trader;
-    	this.year=year;
-    	this.value=value;
-    }
-	public Trader getTrader() {
-		return trader;
+	public void setLastName(String lastName) {
+		LastName = lastName;
 	}
-	public void setTrader(Trader trader) {
-		this.trader = trader;
+	public char getGender() {
+		return gender;
 	}
-	public int getYear() {
-		return year;
+	public void setGender(char gender) {
+		this.gender = gender;
 	}
-	public void setYear(int year) {
-		this.year = year;
-	}
-	public int getValue() {
-		return value;
-	}
-	public void setValue(int value) {
-		this.value = value;
-	}
+	
 }
--- question6.java
+++ question6.java
@@ -1,18 +1,12 @@
-import java.util.Arrays;
-import java.util.List;
 
-@FunctionalInterface
-interface MyFunctionalInterface1 {
-
-	//A method with single parameter
-    public double incrementByFive(double a,double b);
+public class question6 {
+public static void main(String args[])
+{
+	
 }
- public class question6 {
- public static void main(String args[])
- {   
-	 int numbers [] = { 19, 45, 78, 23, 66, 89, 90, 123 }; 
-	 int sum = Arrays.stream(numbers).sum();
-		System.out.print(sum);
- }
 }
 
+class medscheme{
+	int salary;
+	String designation;
+}
