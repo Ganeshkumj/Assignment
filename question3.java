7a68c41e87de3071c63781100087ab5f7c243a6d
Ganeshkumj <102136764+Ganeshkumj@users.noreply.github.com>
2022-05-03 19:58:23 +0530

    java8 assignment
    java8 assignment

--- question3.java
+++ question3.java
@@ -1,48 +1,77 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class question3 {
-public static void main(String args[])
-{
-	person1 p1=new person1("name1","name2",'M');
-	System.out.println("details of person class are mentioned below");
-	System.out.println(p1.getFirstName());
-	System.out.println(p1.getLastName());
-	System.out.println(p1.getGender());
-}
-}
+	public static void main(String args[])
+	{
+	Trader raoul = new Trader("Raoul", "Cambridge");
+	Trader mario = new Trader("Mario","Milan");
+	Trader alan = new Trader("Alan","Cambridge");
+	Trader brian = new Trader("Brian","Cambridge");
 
-class person1{
-	String FirstName;
-	String LastName;
-	char gender;
-	
-	public person1(String firstName, String lastName, char gender) 
-	{
-		super();
-		FirstName = firstName;
-		LastName = lastName;
-		this.gender = gender;
+	List<Transaction> transactions = Arrays.asList(
+		new Transaction(brian, 2011, 300),
+		new Transaction(raoul, 2012, 1000),
+		new Transaction(raoul, 2011, 400),
+		new Transaction(mario, 2012, 710),
+		new Transaction(mario, 2012, 700),
+		new Transaction(alan, 2012, 950)
+	      );
 	}
-	public person1()
-	{
-	System.out.println("this is a default constructor");	
+}
+ class Trader {
+	 
+    private String name;
+    private String city;
+    Trader(String name,String city)
+    {
+    	super();
+    	this.name=name;
+    	this.city=city;
+    }
+	public String getName() {
+		return name;
 	}
-	public String getFirstName() {
-		return FirstName;
+	public void setName(String name) {
+		this.name = name;
 	}
-	public void setFirstName(String firstName) {
-		FirstName = firstName;
+	public String getCity() {
+		return city;
 	}
-	public String getLastName() {
-		return LastName;
+	public void setCity(String city) {
+		this.city = city;
 	}
-	public void setLastName(String lastName) {
-		LastName = lastName;
+
+}
+ class Transaction {
+
+    private Trader trader;
+    private int year;
+    private int value;
+    
+    Transaction(Trader trader,int year,int value)
+    {
+    	super();
+    	this.trader=trader;
+    	this.year=year;
+    	this.value=value;
+    }
+	public Trader getTrader() {
+		return trader;
 	}
-	public char getGender() {
-		return gender;
+	public void setTrader(Trader trader) {
+		this.trader = trader;
 	}
-	public void setGender(char gender) {
-		this.gender = gender;
+	public int getYear() {
+		return year;
 	}
-	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
